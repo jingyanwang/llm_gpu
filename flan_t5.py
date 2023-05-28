683f08ad8233ec0a3c96cea566d660c60680e158
Jingyan Wang <jimjywang@gmail.com>
2023-05-28 11:14:54 -0500

    Add files via upload

---
+++ flan_t5.py
@@ -0,0 +1,49 @@
+import torch
+
+torch.cuda.get_device_name()
+
+torch.cuda.device_count()
+
+device = 0
+
+###
+
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+model = AutoModelForSeq2SeqLM.from_pretrained(
+    "google/flan-t5-base",
+    )
+
+tokenizer = AutoTokenizer.from_pretrained(
+    "google/flan-t5-base",
+    )
+
+
+model = model.to(device)
+
+###
+
+context = "My name is Jim."
+
+question = "What is my name?"
+
+prompt = f"""
+{context}
+
+Q: {question}
+"""
+
+inputs = tokenizer(
+    prompt, 
+    return_tensors="pt")     
+
+inputs = inputs.to(device) if device is not None else inputs
+
+outputs = model.generate(**inputs)
+
+result = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+
+result
+
+while(True):
+    outputs = model.generate(**inputs)
