d7b39c01209a8d293aef8ffe5df414a1adde4844
Jingyan Wang <jimjywang@gmail.com>
2023-05-28 12:58:27 -0500

    Add files via upload

---
+++ bloomz.py
@@ -0,0 +1,57 @@
+import torch
+
+torch.cuda.get_device_name()
+
+torch.cuda.device_count()
+
+device = 5
+
+model_id = "bigscience/bloomz-7b1"
+
+###
+
+print("loading model")
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    )
+
+tokenizer = AutoTokenizer.from_pretrained(
+    model_id,
+    )
+
+
+model = model.to(device)
+
+###
+
+print("inference")
+
+context = "My name is Jim."
+
+question = "What is my name?"
+
+prompt = f"""
+{context}
+
+Question: {question}
+"""
+
+inputs = tokenizer(
+    prompt, 
+    return_tensors="pt")     
+
+inputs = inputs.to(device) if device is not None else inputs
+
+outputs = model.generate(**inputs)
+
+result = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+
+print(result)
+
+print("continues inference")
+
+while(True):
+    outputs = model.generate(**inputs)
